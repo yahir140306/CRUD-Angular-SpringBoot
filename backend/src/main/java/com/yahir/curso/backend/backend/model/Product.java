ad9776cb527b24ee85688141f657ff86a6157ddf
yahir140306 <isidrocej@gmail.com>
2026-04-12 17:47:34 -0600

    feat: add Product entity with fields and getters/setters

--- backend/src/main/java/com/yahir/curso/backend/backend/model/Product.java
+++ backend/src/main/java/com/yahir/curso/backend/backend/model/Product.java
@@ -1,4 +1,48 @@
 package com.yahir.curso.backend.backend.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private Double precio;
+    private Integer cantidad;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
 }
