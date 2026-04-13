ac6caf81e26adf189987b401e5a90ca385e22783
yahir140306 <isidrocej@gmail.com>
2026-04-12 22:37:50 -0600

    feat: implement product management functionality in App component

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,99 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, signal } from '@angular/core';
+import { Product } from './models/model';
+import { ProductService } from './services/product';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('fronted');
+export class App implements OnInit {
+  products: Product[] = [];
+
+  formulario: Product = {
+    name: '',
+    price: 0,
+    quantity: 0,
+  };
+
+  editId: number | null = null;
+
+  mensaje: string = '';
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.loadProducts();
+  }
+
+  loadProducts(): void {
+    this.productService.view().subscribe({
+      next: (data) => {
+        this.products = data;
+      },
+      error: (err) => {
+        this.mensaje = 'Error al cargar productos. Posible error en el backend';
+        console.error(err);
+      },
+    });
+  }
+
+  save(): void {
+    if (this.editId !== null) {
+      this.productService.update(this.editId, this.formulario).subscribe({
+        next: (data) => {
+          this.mensaje = 'Producto actualizado correctamente';
+          this.loadProducts();
+          this.cleanForm();
+        },
+        error: (err) => (this.mensaje = 'Error al actualizar'),
+      });
+    } else {
+      this.productService.create(this.formulario).subscribe({
+        next: (data) => {
+          this.mensaje = 'Producto creado correctamente';
+          this.loadProducts();
+          this.cleanForm();
+        },
+        error: (err) => (this.mensaje = 'Error al crear producto'),
+      });
+    }
+  }
+
+  edit(product: Product): void {
+    this.editId = product.id!;
+    this.formulario = {
+      name: product.name,
+      price: product.price,
+      quantity: product.quantity,
+    };
+
+    this.mensaje = '';
+  }
+
+  delete(id: number): void {
+    if (!confirm('Seguro de eliminar?')) {
+      return;
+    }
+
+    this.productService.delete(id).subscribe({
+      next: (data) => {
+        this.mensaje = 'Producto eliminado';
+        this.loadProducts();
+      },
+      error: (err) => (this.mensaje = 'Error al eliminar'),
+    });
+  }
+
+  cleanForm(): void {
+    this.formulario = {
+      name: '',
+      price: 0,
+      quantity: 0,
+    };
+    this.editId = null;
+    setTimeout(() => (this.mensaje = ''), 3000);
+  }
 }
